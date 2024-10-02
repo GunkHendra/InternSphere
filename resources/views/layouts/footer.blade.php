a3ff3bf41214becaf179624e2452cef252e8abe6
Gung Hendra <gunghencrypt@gmail.com>
2024-10-02 15:44:14 +0800

    Redesign landing page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,48 @@
+<div class="mt-10 h-100vh bg-black">
+    <div class="flex items-center justify-between text-white h-20 p-6">
+        <div class="flex items-center gap-4">
+            <img class="max-w-8 max-h-8" src="assets/link.png" alt="">
+            <span>Connects students with valuable internship opportunities.</span>
+        </div>
+        <div class="flex items-center gap-4">
+            <img class="max-w-12 max-h-12" src="assets/thumb.png" alt="">
+            <span>Find the perfect internship to match your skills and ambitions.</span>
+        </div>
+        <div class="flex items-center gap-4">
+            <img class="max-w-10 max-h-10" src="assets/easy.png" alt="">
+            <span>Easy access to internships.</span>
+        </div>
+    </div>
+
+    <hr class="mx-6 border border-white">
+    <div class="px-6 py-4 text-white space-y-8">
+        
+        <div class="flex justify-center items-center">
+            <div class="items-center space-y-2">
+                <div class="flex justify-center">
+                    <img class="max-w-24 max-h-24" src="assets/logo.png" alt="logo">
+                </div>
+                <h1 class="text-2xl font-bold">{{ config('app.name', 'InternSphere') }}</h1>
+            </div>
+        </div>
+        
+        <ul class="flex justify-center items-center gap-6">
+            <li><a href="/"><img class="max-w-8 max-h-8" src="assets/logo.png" alt="instagram"></a></li>
+            <li><a href="/"><img class="max-w-8 max-h-8" src="assets/logo.png" alt="facebook"></a></li>
+            <li><a href="/"><img class="max-w-8 max-h-8" src="assets/logo.png" alt="twitter"></a></li>
+            <li><a href="/"><img class="max-w-8 max-h-8" src="assets/logo.png" alt="twitter"></a></li>
+            <li><a href="/"><img class="max-w-8 max-h-8" src="assets/logo.png" alt="twitter"></a></li>
+        </ul>
+
+        <div >
+            <ul class="flex justify-between items-center gap-6">
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Customer Support</li>
+                <li>Term & Condition</li>
+                <li>News</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts/layout')
 
 @section('content')
-    <div class="flex flex-row">
-        <div class="basis-1/4 border p-4 mr-4 rounded-lg">
-            Profile Space
+    <div class="flex h-[86vh] justify-center items-center mb-6">
+        <div class="flex justify-between items-center gap-10">
+            <img class="max-w-56 max-h-56" src="assets/logo.png" alt="Logo">
+            <div class="space-y-5">
+                <a href="/about" class="text-8xl font-bold">{{ config('app.name') }}</a>
+                <p class="text-2xl">Where the internship waits for you.</p>
+            </div>
         </div>
-        <div class="basis-3/4">
-            @include('layouts/internship_list')
-        </div>
+    </div>
+    <div>
+        @include('layouts/internship_list')
     </div>
 @endsection
