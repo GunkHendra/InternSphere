91cb178923358067c6f4130ccba66e50a0fc7755
Gung Hendra <gunghencrypt@gmail.com>
2024-10-07 16:11:29 +0800

    redesign landing page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,22 +1,21 @@
-<div class="mt-10 h-100vh bg-black">
+<div class="mt-10 h-100vh bg-black bottom-0">
     <div class="flex items-center justify-between text-white h-20 p-6">
         <div class="flex items-center gap-4">
-            <img class="max-w-8 max-h-8" src="assets/link.png" alt="">
+            <img class="max-w-8 max-h-8" src="assets/icon/link.png" alt="">
             <span>Connects students with valuable internship opportunities.</span>
         </div>
         <div class="flex items-center gap-4">
-            <img class="max-w-12 max-h-12" src="assets/thumb.png" alt="">
+            <img class="max-w-12 max-h-12" src="assets/icon/thumb.png" alt="">
             <span>Find the perfect internship to match your skills and ambitions.</span>
         </div>
         <div class="flex items-center gap-4">
-            <img class="max-w-10 max-h-10" src="assets/easy.png" alt="">
+            <img class="max-w-10 max-h-10" src="assets/icon/easy.png" alt="">
             <span>Easy access to internships.</span>
         </div>
     </div>
 
     <hr class="mx-6 border border-white">
     <div class="px-6 py-4 text-white space-y-8">
-        
         <div class="flex justify-center items-center">
             <div class="items-center space-y-2">
                 <div class="flex justify-center">
@@ -34,8 +33,8 @@
             <li><a href="/"><img class="max-w-8 max-h-8" src="assets/social/telegram.png" alt="telegram"></a></li>
         </ul>
 
-        <div >
-            <ul class="flex justify-between items-center gap-6 mb-6">
+        <div>
+            <ul class="flex justify-between items-center gap-6 mb-2">
                 <li>About Us</li>
                 <li>Contact Us</li>
                 <li>Customer Support</li>
@@ -44,5 +43,4 @@
             </ul>
         </div>
     </div>
-
 </div>
